9c774d745376d3ef36ad50c90cf39b16fdd84cd4
Mattew017 <70763639+Mattew017@users.noreply.github.com>
2021-03-27 13:59:58 +0500

    inital commit

---
+++ Watson.py
@@ -0,0 +1,51 @@
+from ibm_watson import AssistantV2
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+
+ASSISTANT_ID = '29d5f536-595c-449b-8268-fb0c08bd8c1c'
+APIKEY = 'k2HZ7nDmVWepBWQ6jqhfCZ2VicW4N2InnctgYS9SrINW'
+URL = 'https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/72d13037-b6e2-4af5-bd83-e13749154e98'
+WORKSPACE_ID = '57256cdd-c343-410d-86e5-25ecd7f1c476'
+
+
+class Watson:
+    session_is_active = False
+    def __init__(self):
+        self.authenticator = IAMAuthenticator(APIKEY)
+        self.assistant = AssistantV2(
+            version='2021-01-15',
+            authenticator=self.authenticator
+        )
+        self.assistant.set_service_url(URL)
+
+
+
+    def start_session(self):
+        
+        # session starting
+        self.session_is_active = True
+        self.session = self.assistant.create_session(assistant_id=ASSISTANT_ID)
+        self.session_id = self.session.get_result()['session_id']
+        print(f"SESSION  {self.session_id} STARTED")
+        
+
+
+    def send(self, command):
+        try:
+            response = self.assistant.message(
+                assistant_id=ASSISTANT_ID,
+                session_id=self.session_id,
+                input={'text': command}
+            ).get_result()['output']['generic'][0]['text']
+            #print(response)
+            return response
+        except Exception as exp:
+            pass
+            print('Exception catch!', exp)
+    
+    def end_session(self):
+        # session deleting
+        self.session_is_active = False
+        if self.session_id:
+            self.assistant.delete_session(assistant_id=ASSISTANT_ID, session_id=self.session_id)
+            print(f"SESSION {self.session_id} ENDED")
---
+++ main.py
@@ -0,0 +1,40 @@
+import telebot
+import Watson
+
+TELEGRAM_BOT_TOKEN = '1777024279:AAEjnaxv7bZ9MWzZlCyj-MkS6Gu8GhR7QbU'
+
+watson_bot = Watson.Watson()
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start_bot_session(message):
+    watson_bot.start_session()
+    bot.send_message(message.chat.id, "Привет, я бот помощник по ML.")
+
+
+@bot.message_handler(commands=['stop'])
+def start_bot_session(message):
+    watson_bot.end_session()
+    bot.send_message(message.chat.id, "Диалог окончен.")
+
+
+# send text from telegram bot  to Watson and send ansert into telegram bot
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    try:
+        response = watson_bot.send(message.text)
+        bot.send_message(message.chat.id, response)
+    except:
+        if not watson_bot.session_is_active:
+            bot.send_message(message.chat.id, "Напишите /start для запуска бота:")
+        else:
+            bot.send_message(message.chat.id, "Я вас не понял, можете перефразировать?")
+
+
+def main():
+    bot.polling()
+
+
+if __name__ == '__main__':
+    main()
